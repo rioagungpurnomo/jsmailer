ee6ad7bf86b3e579ae39047d2a4c017f53d492bf
rioagungpurnomo <rioagungpurnomo.ak@gmail.com>
2023-05-02 11:30:28 +0700

    Update

--- jsmailer.js
+++ jsmailer.js
@@ -22,16 +22,21 @@ var jsmailer = {
           keySize: keySize,
           iterations: iterations,
         });
-        return [key, 263429942052226];
+        
+        array = {
+          "key": key,
+          "iv": 263429942052226
+        };
+        return array;
   },
   encrypt: function(plaintext){
-    const iv = jsmailer.key()[1];
-    const ciphertext = CryptoJS.AES.encrypt(plaintext, jsmailer.key()[0], { iv: iv });
+    const iv = jsmailer.key().iv;
+    const ciphertext = CryptoJS.AES.encrypt(plaintext, jsmailer.key().key, { iv: iv });
     return ciphertext.toString();
   },
   decrypt: function(ciphertext){
-    const iv = jsmailer.key()[1];
-    const bytes = CryptoJS.AES.decrypt(ciphertext, jsmailer.key()[0], { iv: iv });
+    const iv = jsmailer.key().iv;
+    const bytes = CryptoJS.AES.decrypt(ciphertext, jsmailer.key().key, { iv: iv });
     const plaintext = bytes.toString(CryptoJS.enc.Utf8);
     return plaintext;
   },
